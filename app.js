5d3d62e951ab880fc1b176d9ef25fa5a8bc2a10b
Marcos Moreno <162011225+marcosmorenodev@users.noreply.github.com>
2024-08-05 06:46:15 -0300

    App update
    Added: Native browser printing function & saving link on printing exit in "sessionStorage".
    README updated.

--- app.js
+++ app.js
@@ -3,29 +3,37 @@
 const inputEl = document.getElementById("link-input");
 const convertBtn = document.createElement("button");
 const qrContainer = document.querySelector(".qr__container.hidden");
+const qrImgContainer = document.getElementById("img-container");
 const qrImg = document.getElementById("qr-img"); 
 const downloadBtn = document.getElementById("download-btn");
+const printBtn = document.getElementById("print-btn");
+const errorWrapper = document.getElementById("error-wrapper");
 const errorContainer = document.querySelector(".error__container.hidden");
 const errorEl = document.getElementById("error-msg");
+const message = document.createElement("h1");
 
-//==============================
+//==============================//
+
+const storedLink = sessionStorage.getItem("storedLink");
 
 function createQRCode(userInput) {
-    console.log(userInput)
-    //Since the input has a minLength of 8, this (kind of) prevents the user from writing an URL that doesn't start with "https://"
+    //* Since the input has a minLength of 8, this (kind of) also prevents the user from writing an URL that doesn't start with "https://". Furtherly improving error handling.
 
     if (userInput === "" || userInput.length <= 8 || !userInput.toLowerCase().startsWith("https://")) { 
-        const err = "You need to paste a link or paste a longer one!";
+        const err = "You need to paste/write a valid link!";
         
         handleError(err);
     } 
 
     else {
+        if (qrContainer.contains(message)) { qrContainer.removeChild(message); }
+
         const userLink = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + userInput;
         
-        inputEl.value = "";
+        sessionStorage.setItem("storedLink", userLink);
+
         qrImg.src = userLink;
-        qrContainer.classList.remove("hidden");
+        (qrContainer.classList).remove("hidden");
     }
 }
 
@@ -42,7 +50,7 @@ async function downloadQRCode() {
 
         const link = document.createElement("a");
         link.href = createdURL;
-        link.download = "qr-code.png";
+        link.download = "qr-code.png"; //The downloaded img will be named this
     
         link.click(); 
     }
@@ -50,12 +58,21 @@ async function downloadQRCode() {
     catch(err) { handleError(err.message); }
 }
 
-async function handleError(err) {
-    errorEl.innerHTML = `<i class="fa-solid fa-triangle-exclamation"></i> ${err} <i <i class="fa-solid fa-triangle-exclamation"></i>`;
-    errorContainer.classList.remove("hidden");
+function printQRCode() {
+    const printingArea = qrImgContainer.innerHTML;
+    (document.body).innerHTML = printingArea;
     
-    setTimeout(() => { errorContainer.classList.add("hidden"); }, 4000);
+    window.print();
+    (window.location).reload(); //* On function exiting, the page will reload
+}
+
+async function handleError(err) {
     console.log(err);
+
+    errorEl.innerHTML = `<i class="fa-solid fa-triangle-exclamation"></i> ${err} <i <i class="fa-solid fa-triangle-exclamation"></i>`;
+    (errorWrapper.classList).remove("hidden");
+    
+    setTimeout(() => { (errorWrapper.classList).add("hidden"); }, 3500);
 }
 
 inputEl.addEventListener( "change", (e => createQRCode(e.target.value)) );
@@ -63,3 +80,16 @@ inputEl.addEventListener( "change", (e => createQRCode(e.target.value)) );
 inputEl.addEventListener("click", (e) => { e.target.value = ""; }); //Empties the bar to improve UX (by the user not having to manually delete the link)
 
 downloadBtn.addEventListener("click", downloadQRCode);
+
+printBtn.addEventListener("click", printQRCode);
+
+if (storedLink) { 
+    inputEl.value = sessionStorage.getItem("storedLink"); 
+    createQRCode(inputEl.value);
+    
+    inputEl.value = "";
+    message.textContent = "Your previous QR Code:";
+
+    (message.classList).add("message");
+    qrContainer.insertAdjacentElement("afterbegin", message);
+}
